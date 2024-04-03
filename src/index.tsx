8ad50995e1e78be7b89ab4106c8ae6a22ebe9a21
Luzefiru <ian.deejay.iv@gmail.com>
2024-04-03 20:20:17 +0800

    init

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import { Hono, Context, Env } from 'hono';
+import { renderer } from './renderer';
+
+const app = new Hono();
+
+app.use(renderer);
+
+app.get('/', (c) => {
+  return c.render(<h1>Hello!</h1>);
+});
+
+// Need to type the context for intellisense
+app.get('/tasks', async (c: Context<Env>) => {
+  const { data: tasks, error } = await c.var.supabase.from('tasks').select();
+
+  if (error) {
+    throw error;
+  }
+
+  return c.json(tasks);
+});
+
+export default app;
